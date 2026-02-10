7cabbe5d4eeab884f5057fc68e992e1ad9e63ff6
g-l-mansell <g.l.mansell@hotmail.co.uk>
2026-02-10 14:25:28 +0000

    fix: data files should be git ignored

--- src/battery_trading_model/constants.py
+++ src/battery_trading_model/constants.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from pathlib import Path
+
 
 DATA_DIR = Path(__file__).parent / "data"
 
@@ -38,3 +40,19 @@ class BatteryParameters:
     @property
     def frac_discharged(self):
         return 1 / (1 - self.d_efficiency)
+
+
+# initalise a battery with parameters from the BEIS 2018 report
+# Table 7 New Battery Storage FM Data
+# https://assets.publishing.service.gov.uk/media/5f3cf6c9d3bf7f1b0fa7a165/storage-costs-technical-assumptions-2018.pdf
+DEFAULT_BATTERY_PARAMETERS = BatteryParameters(
+    C_max=50, 
+    c_rate=50, 
+    d_rate=50, 
+    c_efficiency=0.05,
+    d_efficiency=0.05,
+    max_lifetime=15,
+    max_cycles=1500,
+    capex=118_717_286,
+    opex=1_335_001, 
+)
--- src/battery_trading_model/main.py
+++ src/battery_trading_model/main.py
@@ -1,21 +1,5 @@
-
-
-from battery_trading_model.constants import BatteryParameters
+from battery_trading_model.constants import DEFAULT_BATTERY_PARAMETERS
 
 
 if __name__ == "__main__":
-
-    # initalise battery parameters
-    # Source: BEIS 2018 report, Table 7 New Battery Storage FM Data
-    # https://assets.publishing.service.gov.uk/media/5f3cf6c9d3bf7f1b0fa7a165/storage-costs-technical-assumptions-2018.pdf
-    battery_params = BatteryParameters(
-        C_max=50, 
-        c_rate=50, 
-        d_rate=50, 
-        c_efficiency=0.05,
-        d_efficiency=0.05,
-        max_lifetime=15,
-        max_cycles=1500,
-        capex=118_717_286,
-        opex=1_335_001, 
-    )
+    ...
